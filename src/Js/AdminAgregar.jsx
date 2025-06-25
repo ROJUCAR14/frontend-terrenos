0986b93c5fce4b8a4e6a88ed2f3110eda682caa3
ROJUCAR14 <rojucar13@gmail.com>
2025-06-24 20:43:52 -0500

    Agregando boton cerrar sesion

--- src/Js/AdminAgregar.jsx
+++ src/Js/AdminAgregar.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AgregarTerreno from './AgregarTerreno';
 import AdminEditarEliminar from './AdminEditarEliminar';
 import '../Estilizacion/AdminAgregar.css'; // para estilos lado a lado
 
 const AdminAgregar = () => {
   const [propiedadSeleccionada, setPropiedadSeleccionada] = useState(null);
+  const navigate = useNavigate();
+
+  const cerrarSesion = () => {
+    localStorage.removeItem('autenticado');
+    navigate('/');
+  };
 
   return (
     <div className="admin-agregar-container">
-      <AgregarTerreno propiedadEditada={propiedadSeleccionada} limpiarPropiedadEditada={() => setPropiedadSeleccionada(null)} />
+      <div className="admin-header">
+        <button className="logout-button" onClick={cerrarSesion}>Cerrar Sesión</button>
+      </div>
+
+      <AgregarTerreno
+        propiedadEditada={propiedadSeleccionada}
+        limpiarPropiedadEditada={() => setPropiedadSeleccionada(null)}
+      />
       <AdminEditarEliminar onEditar={setPropiedadSeleccionada} />
     </div>
   );
